27301baa8f9ed9f70f12ad271475cfb61fb2a097
Muhammad Tsaroev <tsaroev17@gmail.com>
2025-07-05 20:23:23 +0300

    add: Hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,9 @@
+import { Hero } from '@/sections/Hero'
 import './Home.scss'
 
 const Home = () => {
   return (
-    <section className='hero'>
-      <div className='hero__inner'>
-        <div className='hero__body container'>
-          <h2 className='hero__title'>Почувствуй молодость на вкус</h2>
-          <button className='hero__button'>Подробнее о продукции</button>
-        </div>
-      </div>
-    </section>
+    <Hero />
   )
 }
 
---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import './Hero.scss'
+
+const Hero = () => {
+  const titleId = 'Hero-title'
+
+  return (
+    <section className='hero' aria-labelledby={titleId}>
+      <div className='hero__inner'>
+        <div className='hero__body container'>
+          <h2 className='hero__title' id={titleId}>Почувствуй молодость на вкус</h2>
+          <button className='hero__button'>Подробнее о продукции</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export { Hero }
