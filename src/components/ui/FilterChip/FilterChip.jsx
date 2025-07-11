b5d3f763d5c0203b8e9b48e1ec03f98d1aa912bd
Muhammad Tsaroev <tsaroev17@gmail.com>
2025-07-11 18:57:20 +0300

    add: FilterChip component and filter chips

---
+++ src/components/ui/FilterChip/FilterChip.jsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames'
+import './FilterChip.scss'
+
+const FilterChip = (props) => {
+  const { title, isChecked, onClick } = props
+
+  return (
+    <button
+      type='button'
+      className={classNames('filter-chip__button', {
+        'is-checked': isChecked,
+      })}
+      aria-pressed={isChecked}
+      onClick={onClick}
+      // key={index}
+    >
+      {title}
+    </button>
+  )
+}
+
+export { FilterChip }
--- src/sections/CatalogProducts/CatalogProducts.jsx
+++ src/sections/CatalogProducts/CatalogProducts.jsx
@@ -4,17 +4,25 @@ import { getProducts } from '@/services/products/getProducts'
 import InlineSVG from 'react-inlinesvg'
 import { ProductCard } from '@/components/ui/ProductCard'
 import deffaultImg from '@/assets/images/popular-products/bottles.png'
+import { FilterChip } from '@/components/ui/FilterChip'
 
 const CatalogProducts = () => {
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [errorMessage, setErrorMessage] = useState('')
   const [searchFieldValue, setSearchFieltValue] = useState('')
+  const [categories, setCategories] = useState([])
+  const [productInCategories, setProductInCategories] = useState([])
 
   useEffect(() => {
     getProducts()
       .then((data) => {
         setProducts(data)
+        data.forEach(({ category }) => {
+          setCategories((nowCategory) => [
+            ...new Set([...nowCategory, category]),
+          ])
+        })
       })
       .catch((err) => {
         setErrorMessage(err.message)
@@ -25,8 +33,14 @@ const CatalogProducts = () => {
       })
   }, [])
 
-  const sortedProducts = products.filter(({ title }) => {
-    return title.toLowerCase().includes(searchFieldValue.toLowerCase())
+  const sortedProducts = products.filter(({ title, category }) => {
+    return (
+      (productInCategories.length > 0
+        ? productInCategories.some((productInCategory) => {
+            return productInCategory === category
+          })
+        : true) && title.toLowerCase().includes(searchFieldValue.toLowerCase())
+    )
   })
 
   const titleId = 'Catalog-products-section'
@@ -37,6 +51,20 @@ const CatalogProducts = () => {
 
   const onResetButtonClick = () => {
     setSearchFieltValue('')
+  }
+
+  const onFilterChipClick = (event) => {
+    const { target } = event
+    const categoryName = target.innerText
+
+    target.classList.toggle('is-checked')
+    setProductInCategories((categoriesList) => {
+      return !categoriesList.includes(categoryName)
+        ? [...categoriesList, categoryName]
+        : categoriesList.filter((categoriesItem) => {
+            return categoriesItem !== categoryName
+          })
+    })
   }
 
   return (
@@ -77,6 +105,16 @@ const CatalogProducts = () => {
               />
             </div>
           )}
+        </div>
+        <div className='catalog-products__filter-chips'>
+          {categories.map((category, index) => (
+            <FilterChip
+              title={category}
+              isChecked={false}
+              key={index}
+              onClick={onFilterChipClick}
+            />
+          ))}
         </div>
       </header>
       <div className='catalog-products__body container'>
