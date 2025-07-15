ae3f7030674b18ef1bbf395e97d408ae7f5cf922
Muhammad Tsaroev <tsaroev17@gmail.com>
2025-07-15 16:37:54 +0300

    add: logout button on Profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,19 @@
+import './Profile.scss'
+import { logout } from "@/store/slices/authSlice"
+import { useDispatch } from "react-redux"
+import { useNavigate } from 'react-router-dom'
+
 const Profile = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
   return (
-    <h1>profile</h1>
+    <div className="profile">
+      <button onClick={() => {
+        dispatch(logout())
+        navigate('/')
+      }}>LOGOUT</button>
+    </div>
   )
 }
 
