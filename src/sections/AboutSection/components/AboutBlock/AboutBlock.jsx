50bb27a8d346a8e331410d9b52add6f9dc29349b
Muhammad Tsaroev <tsaroev17@gmail.com>
2025-07-23 00:17:19 +0300

    add: AboutBlock component and About section

---
+++ src/sections/AboutSection/components/AboutBlock/AboutBlock.jsx
@@ -0,0 +1,41 @@
+import './AboutBlock.scss'
+import substruc1 from '@/assets/images/about/subtract-1.svg'
+import substruc2 from '@/assets/images/about/subtract-2.svg'
+import InlineSVG from 'react-inlinesvg'
+import classNames from 'classnames'
+
+const AboutBlock = (props) => {
+  const {
+    /**
+     * 'left' (default) | 'right'
+     */
+    direction = 'left',
+    title,
+    description1,
+    description2,
+    className,
+  } = props
+
+  const substructSVG = (
+    <InlineSVG src={direction === 'left' ? substruc1 : substruc2} />
+  )
+
+  return (
+    <div
+      className={classNames('about__block', className, {
+        [`about__block--${direction}`]: direction,
+      })}
+    >
+      {substructSVG}
+      <h2 className='about__block-title'>{title}</h2>
+      <div className='about__block-description'>
+        <p>
+          {description1} <br />
+          {description2}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export { AboutBlock }
