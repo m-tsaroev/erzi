184dab86996950790f6eff947fb68db5127058a3
Muhammad Tsaroev <tsaroev17@gmail.com>
2025-08-10 15:10:54 +0300

    add: change design

--- src/sections/CartSection/components/OrderSummary/OrderSummary.jsx
+++ src/sections/CartSection/components/OrderSummary/OrderSummary.jsx
@@ -2,22 +2,45 @@ import { formatPrice } from '@/utils/formatPrice'
 import './OrderSummary.scss'
 
 const OrderSummary = (props) => {
-  const {quantity, summ} = props
+  const { quantity, summ, deliveryPrice } = props
+
+  const resultSumm = summ ? summ : 0 + deliveryPrice ? deliveryPrice : 0
 
   return (
-    <div className="order-summary">
-      <h4 className="order-summary__title">Итого</h4>
-      <div className="order-summary__group">
-        <span>Товары:</span>
-        {quantity && <span>{quantity}</span>}
+    <div className='order-summary'>
+      <h4 className='order-summary__title'>Итого</h4>
+      <div className='order-summary__groups'>
+        <div className='order-summary__group'>
+          <span>Товары:</span>
+          {quantity && <span>{quantity}</span>}
+        </div>
+        <div className='order-summary__group'>
+          <span>Сумма:</span>
+          {summ && <span>{formatPrice(summ)} ₽</span>}
+        </div>
+        <div className='order-summary__group'>
+          <span>Доставка:</span>
+          {deliveryPrice ? (
+            <span>{formatPrice(deliveryPrice)}</span>
+          ) : (
+            'Бесплатно'
+          )}
+        </div>
+        <div className='order-summary__group'>
+          <span>Общая сумма:</span>
+          {resultSumm && <span>{formatPrice(resultSumm)} ₽</span>}
+        </div>
       </div>
-      <div className="order-summary__group">
-        <span>Сумма:</span>
-        {summ && <span>{formatPrice(summ)}</span>}
+
+      <button className='order-summary__button'>Заказать</button>
+      <div className='order-summary__extra'>
+        <p>
+          Выбирая один из способов оплаты выше, вы подтверждаете, что
+          ознакомлены, понимаете и соглашаетесь с Условиями использования,
+          Условиями продажи и Политикой возврата, а также признаёте Политику
+          конфиденциальности
+        </p>
       </div>
-      <button className="order-summary__button">
-        Заказать
-      </button>
     </div>
   )
 }
